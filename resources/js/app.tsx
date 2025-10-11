c83504f1629e29bca46b6fd522550859286f038a
titanckw <karikuclemo@gmail.com>
2025-10-11 17:28:23 +0300

    fixes

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,14 +1,16 @@
-import './bootstrap';
+import "./bootstrap";
 
-import { createInertiaApp } from '@inertiajs/react'
-import { createRoot } from 'react-dom/client'
+import { createInertiaApp } from "@inertiajs/react";
+import { createRoot } from "react-dom/client";
 
 createInertiaApp({
-  resolve: name => {
-    const pages = (import.meta as any).glob('./Pages/**/*.jsx', { eager: true })
-    return pages[`./Pages/${name}.jsx`] as Promise<any>
-  },
-  setup({ el, App, props }) {
-    createRoot(el).render(<App {...props} />)
-  },
-})
+    resolve: (name) => {
+        const pages = (import.meta as any).glob("./Pages/**/*.tsx", {
+            eager: true,
+        });
+        return pages[`./Pages/${name}.tsx`] as Promise<any>;
+    },
+    setup({ el, App, props }) {
+        createRoot(el).render(<App {...props} />);
+    },
+});
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    @vite('resources/js/app.js')
-    @inertiaHead
-  </head>
-  <body>
-    @inertia
-  </body>
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  @vite('resources/js/app.tsx')
+  @inertiaHead
+</head>
+
+<body>
+  @inertia
+</body>
+
 </html>
