a3d68bd8a4bee9db38f17bdc65cb00882165277b
titanckw <karikuclemo@gmail.com>
2025-10-11 11:11:58 +0300

    fix: Update Inertia app setup for TypeScript compatibility and add InertiaJS dependency

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -5,8 +5,8 @@ import { createRoot } from 'react-dom/client'
 
 createInertiaApp({
   resolve: name => {
-    const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })
-    return pages[`./Pages/${name}.jsx`]
+    const pages = (import.meta as any).glob('./Pages/**/*.jsx', { eager: true })
+    return pages[`./Pages/${name}.jsx`] as Promise<any>
   },
   setup({ el, App, props }) {
     createRoot(el).render(<App {...props} />)
